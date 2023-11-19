4d9cc63fadd27bbcc58eb89f8d17d4afda27e4bf
Dar1aH <daxundaintravel@gmail.com>
2023-11-19 23:12:09 +0000

    customerFeedbackForm

---
+++ cypress/e2e/customerFeedbackForm.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+it('Customer Feedback Form', ()=>{
+  cy.visit('/'); 
+  cy.get('button[aria-label="Close Welcome Banner"]').click();
+  cy.get('button[aria-label="Open Sidenav"]').click();
+  cy.get('a[aria-label="Go to contact us page"]').click();
+  cy.get('#comment').type('Nice products. Awesome selection.Thanks a lot!')
+
+  cy.get('#rating').trigger('mousedown', { which: 1 })
+  .trigger('mousemove', { clientX: 100 })
+  .trigger('mouseup', { force: true })
+})
